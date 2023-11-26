22f8d292d843ff4448543fcef084d25e700be9e0
Eugene <marycheugene@gmail.com>
2023-11-26 12:38:00 +0100

    Updates

--- cleaning.7de2d514.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=cleaning.7de2d514.js.map
---
+++ cleaning.8b6f37fa.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".smoothsclr").forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var t=this.getAttribute("href");document.querySelector(t).scrollIntoView({behavior:"smooth"})})});
+//# sourceMappingURL=cleaning.8b6f37fa.js.map
