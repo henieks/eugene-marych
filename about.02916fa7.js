17a6a2529889e40519cced72a9d366c7109adb2f
Eugene <marycheugene@gmail.com>
2024-08-25 18:00:00 +0200

    Updates

--- about.02916fa7.js
+++ about.02916fa7.js
@@ -1,2 +1,2 @@
 document.querySelectorAll(".smoothsclr").forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var t=this.getAttribute("href");document.querySelector(t).scrollIntoView({behavior:"smooth"})})});
-//# sourceMappingURL=archive.56bd74e8.js.map
+//# sourceMappingURL=about.02916fa7.js.map
