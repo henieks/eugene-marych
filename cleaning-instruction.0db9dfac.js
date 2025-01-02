1abf3dfefa17c74b4c1c969b96173843001dd6e3
henieks <63364682+henieks@users.noreply.github.com>
2025-01-02 14:22:19 +0100

    Updates

--- cleaning-instruction.0db9dfac.js
+++ cleaning-instruction.0db9dfac.js
@@ -1,2 +1,2 @@
 document.querySelectorAll(".smoothsclr").forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var t=this.getAttribute("href");document.querySelector(t).scrollIntoView({behavior:"smooth"})})});
-//# sourceMappingURL=about.02916fa7.js.map
+//# sourceMappingURL=cleaning-instruction.0db9dfac.js.map
