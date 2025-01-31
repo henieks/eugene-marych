ffa64c2b12ae5a1ab718715d746925e6d75082db
henieks <63364682+henieks@users.noreply.github.com>
2025-01-31 17:30:25 +0100

    Updates

--- cleaning-instruction.0db9dfac.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".smoothsclr").forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var t=this.getAttribute("href");document.querySelector(t).scrollIntoView({behavior:"smooth"})})});
-//# sourceMappingURL=cleaning-instruction.0db9dfac.js.map
---
+++ index.12163a6d.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",()=>{document.body.classList.add("loaded")});
+//# sourceMappingURL=index.12163a6d.js.map
